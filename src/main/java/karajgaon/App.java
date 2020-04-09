e390658d85661ba2103681eb6b50def016c78441
pdppatil <pdppatil@gmail.com>
2020-04-09 12:03:56 +0530

    new prject added

---
+++ src/main/java/karajgaon/App.java
@@ -0,0 +1,13 @@
+package karajgaon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
